bab0f029f3a22dde78ddeb0a785e8c6bd75e4176
Israa Kamel <israa.almarzooqi-1@hotmail.com>
2023-02-23 18:16:41 +0400

    Initial Commit

---
+++ Collabration Demo/Collabration_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Collabration_DemoApp.swift
+//  Collabration Demo
+//
+//  Created by apple on 23/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct Collabration_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
